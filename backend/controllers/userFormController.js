df7ea92d8e67bea8a493f4131723a884e062e6eb
piekarate <92880613+piekarate@users.noreply.github.com>
2022-10-26 19:08:47 +0100

    User controller an routes setup

--- backend/controllers/userFormController.js
+++ backend/controllers/userFormController.js
@@ -1,24 +1,24 @@
 const asyncHandler = require('express-async-handler')
 
-const getUserForms = async (req, res) => {
+const getUserForms = asyncHandler( async (req, res) => {
     res.status(200).json({message: 'Get forms'})
-} 
+} )
 
-const postUserForms = async (req, res) => {
+const postUserForms = asyncHandler( async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Add a text field')
     }
     res.status(200).json({message: 'Set forms'})
-} 
+} )
 
-const updateUserForms = async (req, res) => {
+const updateUserForms = asyncHandler( async (req, res) => {
     res.status(200).json({message: `Update form ${req.params.id}`})
-} 
+} )
 
-const deleteUserForms = async (req, res) => {
+const deleteUserForms = asyncHandler( async (req, res) => {
     res.status(200).json({message: `Delete form ${req.params.id}`})
-} 
+} )
 
 
 module.exports = {
