85c74be74ec956bc88bba11c9e67f0fb988b52eb
piekarate <92880613+piekarate@users.noreply.github.com>
2022-11-03 21:51:24 +0000

    Addeduser functionality (Log In, Register)

--- backend/models/formModel.js
+++ backend/models/formModel.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
     name: {
-        type: String,
-        required: [true, 'Please provide a name']
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     },
     text: {
         type: String,
--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/forms', require('./routes/userFormRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
